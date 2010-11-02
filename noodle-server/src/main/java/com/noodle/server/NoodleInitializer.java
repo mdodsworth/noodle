6a7ad8bb1638a056f2b147ade6601df8f939d315
mdodsworth <dodster.michael@googlemail.com>
2010-11-02 00:42:41 -0400

    creating placeholder for noodle initialization

---
+++ noodle-server/src/main/java/com/noodle/server/NoodleInitializer.java
@@ -0,0 +1,21 @@
+package com.noodle.server;
+
+/**
+ * Entry point for server initialization
+ *
+ * @author mdodsworth
+ */
+public final class NoodleInitializer {
+
+    //======== main() method ========//
+
+    /**
+     * Initialize the noodle context and bootstrap the server
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // initialize guice
+    }
+}
